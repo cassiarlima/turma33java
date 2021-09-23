db67a6c345684472133c6e2a7132a3d05bceb03d
cassiarlima <cassiadosreislima@gmail.com>
2021-09-23 19:11:33 -0300

    Meu primeiro Spring

---
+++ hello (1)/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		return "Hello, turma 33!";
+	}
+	
+	
+}
